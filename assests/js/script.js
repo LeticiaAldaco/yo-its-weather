3e8e98186d3098b5844b19a95216a08ad1096df6
Leticia Aldaco <leticiaaldaco@leticias-mbp.lan>
2021-04-29 14:31:22 -0500

    organized files

---
+++ assests/js/script.js
@@ -0,0 +1,10 @@
+var currentUrl = "api.openweathermap.org/data/2.5/weather?q=";
+var uvUrl = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}";
+var apiKey = "&appid=feae67b17a7db2b1dc50bc7b73015b2c&units=imperial";
+var forecastUrl = "api.openweathermap.org/data/2.5/forecast?q=";
+
+// Grab the city name from input and create an event listener for the search button
+
+// Create the variable to grab the city name from the input 
+
+// Creat function for current weather API call with city peramater
