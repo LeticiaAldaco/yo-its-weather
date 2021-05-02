3eb10940b5420f64495df58444a7b803cddd0a46
Leticia Aldaco <leticiaaldaco@leticias-mbp.lan>
2021-05-02 11:13:12 -0500

    looped through each card element

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,6 +34,7 @@ function renderCurrentWeather(data) {
   var cityHumid = document.querySelector("#cityHumid");
   var cityHeader = document.querySelector("#cityHeader");
   var uvInfo = document.querySelector("#uvInfo");
+  
   cityTemp.textContent = "Temp: " + data.main.temp;
   cityWind.textContent = "Wind: " + data.wind.speed + " MPH";
   cityHumid.textContent = "Humidity: " + data.main.humidity + "%";
@@ -51,9 +52,8 @@ function renderCurrentWeather(data) {
       return response.json();
     })
     .then(function (data) {
-       console.log(data);
-       uvInfo.textContent = "UV Index: " + data.value;
-
+      console.log(data);
+      uvInfo.textContent = "UV Index: " + data.value;
     });
 }
 function renderFivedayForecast(data) {
@@ -65,14 +65,17 @@ function renderFivedayForecast(data) {
     })
     .then(function (data) {
       console.log(data);
-      for(let i = 1; i< 6; i ++){
-       let divId = $("div#" + i)
-divId.children()[0].innerHTML = data.list[(i*8)-1].dt_txt.split(" ")[0];
-divId.children()[1].innerHTML = data.list[(i*8)-1].weather.icon;
-// divID.children()
+      for (let i = 1; i < 6; i++) {
+        let divId = $("div#" + i);
+        divId.children()[0].innerHTML = data.list[i * 8 - 1].dt_txt.split(" ")[0];
+        divId.children()[1].setAttribute("src", `https://openweathermap.org/img/w/${data.list[i * 8 - 1].weather[0].icon}.png`); 
+      console.log(data.list[i * 8 - 1].weather[0].icon);
+        divId.children()[2].innerHTML = "Temp: " + data.list[i * 8 - 1].main.temp;
+        divId.children()[3].innerHTML = "Wind: " + data.list[i * 8 - 1].wind.speed + " MPH";
+        divId.children()[4].innerHTML = "Humidity: " + data.list[i * 8 - 1].main.humidity;
       }
+
     });
-//     document.querySelector("#");
 }
 
 form.addEventListener("submit", returnCityInfo);
